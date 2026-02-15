81b1f8b067dbf0bd889c8ce34e2958a863400965
natnael-eyuel-dev <natnael.eyuel.dev@gmail.com>
2026-02-15 20:56:56 +0300

    chore: add shared icons

---
+++ src/components/shared/icons.tsx
@@ -0,0 +1,23 @@
+import {
+  Loader2,
+  User,
+  Github,
+  Chrome,
+  X,
+  Check,
+  Key,
+  Trash2,
+  AlertTriangle 
+} from "lucide-react";
+
+export const Icons = {
+  spinner: Loader2,
+  user: User,
+  gitHub: Github,
+  google: Chrome,
+  x: X,
+  check: Check,
+  key: Key,
+  trash2: Trash2,
+  alertTriangle: AlertTriangle 
+};
